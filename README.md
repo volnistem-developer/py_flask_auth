f08f3fcbea63eca46bc7b7674c6b616457352da8
Iuri Volnistem <iurivolnistem@gmail.com>
2025-10-23 20:58:02 -0300

    User CRUD finished

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # py_flask_auth
+
+### projeto criado com a intenção de aprender sobre autenticação com Flask e utilizar conceitos como ORM usando SQLite.
---
+++ models/user.py
@@ -0,0 +1,8 @@
+from database import db
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    # id (int), username (str), password (str)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
